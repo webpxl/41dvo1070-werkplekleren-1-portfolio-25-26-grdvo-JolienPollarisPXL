a6afda2291df84c9def681befa8fd4e71118e515
Jolien Pollaris <jolien.pollaris@student.pxl.be>
2026-01-08 21:20:47 +0100

    FEAT 2 - made home page - made pages and linked them to each other - added js array (work-experience) - added images - added layout to homepage

---
+++ portfolio/Portfolio ervaring & skills.js
@@ -0,0 +1,40 @@
+const werkervaring = [
+    {
+    function: "Medewerker schoenenwinkel (jobstudent)",
+    imgPath:  "/images/ervaring/schoenenwinkel.jpg",
+    location: "Pronti (Tielt-Winge)",
+    date: "6/08/2023-heden",
+    },
+    {
+        function: "Redactie Bedrijvig Limburg & dienst communicatie (stage)",
+        imgPath:  "/images/ervaring/schoenenwinkel.jpg",
+        location: "Voka - KvK Limburg (Hasselt)",
+        date: "17/02/2025-23/05/2025",
+    },
+    {
+        function: "Dienst communicatie(stage)",
+        imgPath:  "/images/ervaring/klantenservice.jpg",
+        location: "Gemeentehuis Kortenaken",
+        date: "06/08/2024-24/05/2024",
+    },
+    {
+        function: "Deur-aan-deur verkoper (jobstudent)",
+        imgPath:  "/images/ervaring/klantenservice.jpg",
+        location: "Activate (Hasselt)",
+        date: "07/07/2022-20/07/2022",
+    },
+    {
+        function: "Kuishulp (jobstudent)",
+        imgPath:  "/images/ervaring/klantenservice.jpg",
+        location: "Amber Ijs (Tielt-Winge)",
+        date: "20/04/2021-17/07/2022",
+    },
+    {
+        function: "Cold Caller (stage)",
+        imgPath:  "/images/ervaring/klantenservice.jpg",
+        location: "D-motion (Hasselt)",
+        date: "27/04/2021-8/05/2021",
+    },
+
+
+];
